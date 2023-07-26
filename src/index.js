27df35f39998438076f0d058c6780b4f8cc4fc85
Arttu Laitinen <124583634+Arttu73@users.noreply.github.com>
2023-07-26 18:39:37 +0000

    Pull

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-
+import "./styles.css";
+import "./leaflet.css";
+import L from "./leaflet";
 import "./styles.css";
 
 const fetchData = async () => {
-    const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326 "
+    try {
+        const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326 "
     const dataResponse = await fetch(url)
     const dataGSON = await dataResponse.json()
-
+    
     initMap(dataGSON)
+    } catch (error) {
+        console.error("Error fetching or parsing data:", error);
+    }
+    
 };
 
 const initMap = (data) => {
-    let map = L.map('map')
+    let map = L.map('map', {
+        minZoom: -3
+    });
+ 
 
     let geoJSON = L.geoJSON(data, {
         weight: 2
     }).addTo(map);
 
     let osm = L.tilelayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    minZoom: -3,
+    maxZoom: 19,
     attribution: '© OpenStreetMap'
     }).addTo(map);
 
-    map.fitBounds(geoJSON.getBounds())
+    map.fitBounds(geoJSON.getBounds());
+    
 }
 
 fetchData();
